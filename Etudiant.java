d215038d43a08e9b2a7895cce47ec9a3af9ba25c
Rami <ghaouarrami@outlook.com>
2021-03-30 01:45:49 +0200

    second change attributs

--- Etudiant.java
+++ Etudiant.java
@@ -2,5 +2,9 @@
 public class Etudiant {
 	private int id ; 
 	private String nom  ; 
+	private String adresse; 
+	private int telephone ; 
+	private String login ; 
+	private String password ; 
 
 }
